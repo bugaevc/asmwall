59d3c08b82085690d4084055c577894db8a09284
Sergey Bugaev <bugaevc@gmail.com>
2015-03-27 00:09:14 +0300

    Fetch instructions.md and dynamically write its contents to the page
    Import jQuery and use it instead of vanillaJS. I still like it.

--- load_content.js
+++ load_content.js
@@ -1,4 +1,25 @@
-window.onload = function(){
-    var instructionsTable = document.getElementById("instructions")
-    instructionsTable.insertRow(0).insertCell(0).innerHTML = "Hello World!";
-};
+$(document).ready(function() {
+    $in = $("#instructions");
+    $.get("data/instructions.md", function(data) {
+            var arr = data.split("\n");
+            var $list = undefined;
+            for(var i = 0; i < arr.length; i++)
+            {
+                arr[i] = $.trim(arr[i]);
+                if(arr[i][0] === '=')
+                {
+                    $list = $("<ul></ul>")
+                    $("<h2></h2>")
+                        .text(arr[i-1])
+                        .appendTo($in)
+                        .after($list);
+                }
+                if(arr[i][0] === '#')
+                {
+                    var content = arr[i].replace(/#/gi, '').replace(/`/gi, '');
+                    content = $.trim(content);
+                    $("<li></li>").text(content).appendTo($list);
+                }
+            }
+        });
+});
