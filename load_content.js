4fdbf44119046e9a7431316b3de68996f9bd87a0
Sergey Bugaev <bugaevc@gmail.com>
2015-04-06 22:26:54 +0300

    Support multiple-level headers in the parser

--- load_content.js
+++ load_content.js
@@ -14,6 +14,35 @@ $(document).ready(function() {
         $in.addClass("instructions");
         var $list = undefined;
     }
+    function addItem(text, level) {
+        switch(level) {
+            case 1:
+                $("<h2></h2>")
+                    .text(text)
+                    .appendTo($in);
+                break;
+
+            case 2:
+                $list = $("<ul></ul>")
+                $("<h3></h3>")
+                    .text(text)
+                    .appendTo($in)
+                    .after($list);
+                break;
+
+            case 3:
+                var $a = $("<a></a>")
+                    .addClass("block-link");
+                var href = '?' + text.replace(/ /g, '_');
+                $a
+                    .attr("href", href)
+                    .text(text);
+                $('<li></li>')
+                    .append($a)
+                    .appendTo($list);
+                break;
+        }
+    }
     $.get("data/instructions.md", function(data) {
             var arr = data.split("\n");
             for(var i = 0; i < arr.length; i++)
@@ -21,13 +50,7 @@ $(document).ready(function() {
                 arr[i] = $.trim(arr[i]);
                 if(arr[i][0] === '=')
                     if(showAll)
-                    {
-                        $list = $("<ul></ul>")
-                        $("<h2></h2>")
-                            .text(arr[i-1])
-                            .appendTo($in)
-                            .after($list);
-                    }
+                        addItem(arr[i-1], 1);
                     else if(isCurrent)
                     {
                         isCurrent = false;
@@ -37,21 +60,12 @@ $(document).ready(function() {
                     }
                 if(arr[i][0] === '#')
                 {
+                    var level = arr[i].indexOf(' ');
                     var content = arr[i].replace(/#/g, '').replace(/`/g, '');
                     content = $.trim(content);
                     if(showAll)
-                    {
-                        var $a = $("<a></a>")
-				.addClass("block-link");
-                        var href = '?' + content.replace(/ /g, '_');
-                        $a
-                            .attr("href", href)
-                            .text(content);
-                        $('<li></li>')
-                            .append($a)
-                            .appendTo($list);
-                    }
-                    else
+                        addItem(content, level);
+                    else if(level === 3)
                         isCurrent = (command === content);
                 }
                 if(!showAll && isCurrent)
