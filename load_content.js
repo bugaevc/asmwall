075281def994eee874af30156f825e1943e0c3bf
Sergey Bugaev <bugaevc@gmail.com>
2016-01-18 20:02:32 +0300

    Refactoring

--- load_content.js
+++ load_content.js
@@ -2,62 +2,65 @@ $in = $("#content");
 var initialTitle = "asmwall - NASM handbook - A Wall to Remember";
 function renderPage(command) {
     $in.empty();
+    function forHeader(line, f) {
+        if (line[0] !== '#')
+            return false;
+        var content = line.replace(/#/g, '').replace(/`/g, '');
+        var level = line.indexOf(' ');
+        f($.trim(content), level);
+        return true;
+    }
     if (command) {
         document.title = command + ' - ' + initialTitle;
-
         $in.removeClass("instructions");
         var md = ""; // markdown code of the current section
         var isCurrent = false; // whether we are inside the needed section
         lines.forEach(function (line) {
-            if (line[0] === '#') {
-                var content = line.replace(/#/g, '').replace(/`/g, '');
-                isCurrent = (command === $.trim(content));
-            }
+            forHeader(line, function (content) {
+                isCurrent = (command === content);
+            });
             if (isCurrent)
                 md += line + '\n';
         });
         if (md)
             $in.html(marked(md));
         else
-            $.get("404.md", function(data2) {
+            $.get("404.md", function(data) {
                 document.title = "404 - " + initialTitle;
-                $in.html(marked(data2));
+                $in.html(marked(data));
             });
     } else {
         document.title = initialTitle;
         $in.addClass("instructions");
         var $list = $("<ul></ul>");
         lines.forEach(function (line) {
-            if (line[0] !== '#')
-                return;
-            var level = line.indexOf(' ');
-            var content = line.replace(/#/g, '').replace(/`/g, '');
-            content = $.trim(content);
-            switch (level) {
-                case 1:
-                case 2:
-                    if (!$list.is(":empty"))
-                        $list = $("<ul></ul>");
-                    var headerTag = '<h' + (level+1) + '>' +
-                                    '</h' + (level+1) + '>';
-                    $(headerTag)
-                        .text(content)
-                        .appendTo($in)
-                        .after($list);
-                    break;
+            forHeader(line, function(content, level) {
+                switch (level) {
+                    case 1:
+                    case 2:
+                        if (!$list.is(":empty"))
+                            $list = $("<ul></ul>");
+                        var headerTag = '<h' + (level+1) + '>' +
+                                        '</h' + (level+1) + '>';
+                        $(headerTag)
+                            .text(content)
+                            .appendTo($in)
+                            .after($list);
+                        break;
 
-                case 3:
-                    var $a = $("<a></a>")
-                        .addClass("block-link");
-                    var href = '?' + content.replace(/ /g, '_');
-                    $a
-                        .attr("href", href)
-                        .text(content);
-                    $('<li></li>')
-                        .append($a)
-                        .appendTo($list);
-                    break;
-            }
+                    case 3:
+                        var $a = $("<a></a>")
+                            .addClass("block-link");
+                        var href = '?' + content.replace(/ /g, '_');
+                        $a
+                            .attr("href", href)
+                            .text(content);
+                        $('<li></li>')
+                            .append($a)
+                            .appendTo($list);
+                        break;
+                }
+            });
         });
     }
 }
